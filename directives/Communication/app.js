1d7387973149c683294bd93d704befc01c26d96e
Astha Sharma <a@astha.me>
2016-02-14 09:42:43 +0530

    Multiple instances of directives

--- directives/Communication/app.js
+++ directives/Communication/app.js
@@ -11,15 +11,21 @@ app.directive("country", function() {
 });
 app.directive("state", function(){
 	return {
-		restrict: 'E'
+		restrict: 'E',
+		controller: function(){
+			this.makeLaw = function(law){
+				console.log("Law:" + law);
+			}
+		}
 	}
 })
 app.directive("city", function(){
 	return {
 		restrict: 'E',
-		require: "^country",
-		link : function(scope, elements, attrs, countryCtrl){
-		countryCtrl.makeAnnouncement("This city rocks");
+		require: ["^country", "^state"],
+		link : function(scope, elements, attrs, ctrls){
+		ctrls[0].makeAnnouncement("This city rocks");
+		ctrls[1].makeLaw("Jump higher");
 		}
 	}
 })
